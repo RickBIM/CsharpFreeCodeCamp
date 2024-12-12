98d8e5c4378ba86daa40ed280fdc5cf314d324a2
RickBIM <hr.furlaneto@gmail.com>
2024-12-12 07:49:20 -0300

    Using comments

--- Program.cs
+++ Program.cs
@@ -1,8 +1,22 @@
-﻿string[] IDs = {"B123","C234","A345","C15","B177","G3003","C235","B179"};
-foreach (string ID in IDs)
+﻿/*
+  The following code creates five random OrderIDs
+  to test the fraud detection process.  OrderIDs 
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123.
+*/
+Random random = new Random();
+string[] orderIDs = new string[5];
+
+for (int i = 0; i < orderIDs.Length; i++)
 {
-    if (ID.StartsWith("B"))
-    {
-        Console.WriteLine($"The {ID} may be fraudulent");
-    }
+    int prefixValue = random.Next(65, 70);
+    string prefix = Convert.ToChar(prefixValue).ToString();
+    string suffix = random.Next(1, 1000).ToString("000");
+
+    orderIDs[i] = prefix + suffix;
+}
+
+foreach (var orderID in orderIDs)
+{
+    Console.WriteLine(orderID);
 }
