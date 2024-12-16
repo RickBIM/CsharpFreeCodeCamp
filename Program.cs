fa4d94da24650fcfc9443cb6ca1e294d43db96e0
RickBIM <hr.furlaneto@gmail.com>
2024-12-15 21:55:43 -0300

    boolean expressions

--- Program.cs
+++ Program.cs
@@ -1,23 +1,28 @@
-﻿/*
-   This code reverses a message, counts the number of times 
-   a particular character appears, then prints the results
-   to the console window.
- */
-string str = "The quick brown fox jumps over the lazy dog.";
-
-char[] charMessage = str.ToCharArray();
-Array.Reverse(charMessage);
-
-int letterCount = 0;
-foreach (char i in charMessage) 
-{ 
-    if (i == 'o') 
-    { 
-        letterCount++; 
-    }
-}
-
-string new_message = new String(charMessage);
-
-Console.WriteLine(new_message);
-Console.WriteLine($"'o' appears {letterCount} times.");
+﻿var headsOrTails = new Random().Next(0,2) == 0 ? "head": "tail";
+Console.WriteLine($"{headsOrTails}");
+headsOrTails = new Random().Next(0,2) == 0 ? "head": "tail";
+Console.WriteLine($"{headsOrTails}");
+headsOrTails = new Random().Next(0,2) == 0 ? "head": "tail";
+Console.WriteLine($"{headsOrTails}");
+headsOrTails = new Random().Next(0,2) == 0 ? "head": "tail";
+Console.WriteLine($"{headsOrTails}");
+headsOrTails = new Random().Next(0,2) == 0 ? "head": "tail";
+Console.WriteLine($"{headsOrTails}");
+headsOrTails = new Random().Next(0,2) == 0 ? "head": "tail";
+Console.WriteLine($"{headsOrTails}");
+headsOrTails = new Random().Next(0,2) == 0 ? "head": "tail";
+Console.WriteLine($"{headsOrTails}");
+headsOrTails = new Random().Next(0,2) == 0 ? "head": "tail";
+Console.WriteLine($"{headsOrTails}");
+headsOrTails = new Random().Next(0,2) == 0 ? "head": "tail";
+Console.WriteLine($"{headsOrTails}");
+headsOrTails = new Random().Next(0,2) == 0 ? "head": "tail";
+Console.WriteLine($"{headsOrTails}");
+headsOrTails = new Random().Next(0,2) == 0 ? "head": "tail";
+Console.WriteLine($"{headsOrTails}");
+headsOrTails = new Random().Next(0,2) == 0 ? "head": "tail";
+Console.WriteLine($"{headsOrTails}");
+headsOrTails = new Random().Next(0,2) == 0 ? "head": "tail";
+Console.WriteLine($"{headsOrTails}");
+headsOrTails = new Random().Next(0,2) == 0 ? "head": "tail";
+Console.WriteLine($"{headsOrTails}");
