c0c6dc117aa1c17b4f124721229f993d2b868351
RickBIM <hr.furlaneto@gmail.com>
2024-12-22 13:42:37 -0300

    do-while Challange

--- Program.cs
+++ Program.cs
@@ -1,14 +1,30 @@
-﻿for (int i = 1; i <= 100; i++)
+﻿int monsterLife = 10;
+int heroLife = 10;
+do
 {
-    if (i % 3 == 0 || i % 5 == 0){
-        Console.Write($"{i} - ");
-        if (i % 3 == 0)
-            Console.Write("Fizz");
-        if (i % 5 == 0)
-            Console.Write("Buzz");
+    int heroAtack = new Random().Next(0,10);
+    if (monsterLife > heroAtack)
+    {
+        monsterLife -= heroAtack;
+        Console.WriteLine($"Monster was damaged and lost {heroAtack} health and now has {monsterLife} health");
     }else
     {
-        Console.Write(i);
+        monsterLife = 0;
+        Console.WriteLine($"Monster was damaged and lost {heroAtack} health and now has {monsterLife} health");
+        Console.WriteLine("Hero wins");
+        break;
     }
-    Console.WriteLine();
-}
+
+    int monsterAtack = new Random().Next(0,10);
+    if (heroLife > monsterAtack)
+    {
+        heroLife -= monsterAtack;
+        Console.WriteLine($"Hero was damaged and lost {monsterAtack} health and now has {heroLife} health");
+    }else
+    {
+        heroLife = 0;
+        Console.WriteLine($"Hero was damaged and lost {monsterAtack} health and now has {heroLife} health");
+        Console.WriteLine("Monster wins");
+        break;
+    }
+}while (monsterLife > 0 && heroLife > 0);
