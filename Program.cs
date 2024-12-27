5e2ed4d451617638bad32310362e5984e51f3145
RickBIM <hr.furlaneto@gmail.com>
2024-12-26 23:24:07 -0300

    Converting strings into int using TryParse()

--- Program.cs
+++ Program.cs
@@ -1,30 +1,5 @@
-﻿int monsterLife = 10;
-int heroLife = 10;
-do
-{
-    int heroAtack = new Random().Next(0,10);
-    if (monsterLife > heroAtack)
-    {
-        monsterLife -= heroAtack;
-        Console.WriteLine($"Monster was damaged and lost {heroAtack} health and now has {monsterLife} health");
-    }else
-    {
-        monsterLife = 0;
-        Console.WriteLine($"Monster was damaged and lost {heroAtack} health and now has {monsterLife} health");
-        Console.WriteLine("Hero wins");
-        break;
-    }
-
-    int monsterAtack = new Random().Next(0,10);
-    if (heroLife > monsterAtack)
-    {
-        heroLife -= monsterAtack;
-        Console.WriteLine($"Hero was damaged and lost {monsterAtack} health and now has {heroLife} health");
-    }else
-    {
-        heroLife = 0;
-        Console.WriteLine($"Hero was damaged and lost {monsterAtack} health and now has {heroLife} health");
-        Console.WriteLine("Monster wins");
-        break;
-    }
-}while (monsterLife > 0 && heroLife > 0);
+﻿string myIntString = "hello";
+if (int.TryParse(myIntString, out int myIntNumber))
+	Console.WriteLine(myIntNumber);
+else
+	Console.WriteLine("Não foi possivel converter");
