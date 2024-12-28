ea94a02fe78bfcd270652d4114aa7461ac9867bd
RickBIM <hr.furlaneto@gmail.com>
2024-12-28 19:19:08 -0300

    Cresting methods with parameters

--- Program.cs
+++ Program.cs
@@ -1,19 +1,13 @@
-﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+﻿string[] students = {"Jenna", "Ayesha", "Carlos", "Viktor"};
 
-string quantity = "";
-string output = "";
+DisplayStudents(students);
+DisplayStudents(new string[] {"Robert","Vanya"});
 
-// Your work here
-
-int startPosition  = input.IndexOf("<span>") + "<span>".Length;
-int endPosition = input.IndexOf("</span>");
-int length = endPosition - startPosition;
-quantity = input.Substring(startPosition, length);
-Console.WriteLine(quantity);
-
-startPosition  = input.IndexOf("<div>") + "<div>".Length;
-endPosition = input.IndexOf("</div>");
-length = endPosition - startPosition;
-output = input.Substring(startPosition, length);
-output = output.Replace("&trade", "&reg");
-Console.WriteLine(output);
+void DisplayStudents(string[] students) 
+{
+    foreach (string student in students) 
+    {
+        Console.Write($"{student}, ");
+    }
+    Console.WriteLine();
+}
