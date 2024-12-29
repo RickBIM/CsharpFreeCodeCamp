b4c1c0f59317b5543ed577d38a24759bfa80b42e
RickBIM <hr.furlaneto@gmail.com>
2024-12-29 11:33:00 -0300

    email creator challenge

--- Program.cs
+++ Program.cs
@@ -1,13 +1,37 @@
-﻿string[] students = {"Jenna", "Ayesha", "Carlos", "Viktor"};
-
-DisplayStudents(students);
-DisplayStudents(new string[] {"Robert","Vanya"});
-
-void DisplayStudents(string[] students) 
+﻿string[,] corporate =
 {
-    foreach (string student in students) 
-    {
-        Console.Write($"{student}, ");
-    }
-    Console.WriteLine();
+    {"Robert", "Bavin"},
+    {"Simon", "Bright"},
+    {"Kim", "Sinclair"},
+    {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"},
+    {"Sinan", "Ali"}
+};
+
+string[,] external =
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++)
+{
+    DisplayCorporateEmail(corporate[i, 0], corporate[i, 1]);
+}
+
+for (int i = 0; i < external.GetLength(0); i++)
+{
+    DisplayExternalEmail(external[i, 0], external[i, 1]);
+}
+
+void DisplayCorporateEmail(string name, string lastName)
+{
+    Console.WriteLine($"{name.Substring(0, 2).ToLower()}{lastName.ToLower()}@contoso.com");
+}
+
+void DisplayExternalEmail(string name, string lastName)
+{
+    Console.WriteLine($"{name.Substring(0, 2).ToLower()}{lastName.ToLower()}@contoso.com");
 }
