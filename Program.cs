d3d9c0a89950c49b11ac68bec6ded53247c09128
RickBIM <hr.furlaneto@gmail.com>
2024-12-30 15:45:07 -0300

    Exception handling challenge

--- Program.cs
+++ Program.cs
@@ -1,46 +1,66 @@
-﻿checked
+﻿string[][] userEnteredValues = new string[][]
 {
-    try
-    {
-        int num1 = int.MaxValue;
-        int num2 = int.MaxValue;
-        int result = num1 + num2;
-        Console.WriteLine("Result: " + result);
-    }
-    catch (OverflowException ex)
-    {
-        Console.WriteLine("Error: The number is too large to be represented as an integer." + ex.Message);
-    }
-}
+            new string[] { "1", "2", "3"},
+            new string[] { "1", "two", "3"},
+            new string[] { "0", "1", "2"}
+};
+
 try
 {
-    string str = null;
-    int length = str.Length;
-    Console.WriteLine("String Length: " + length);
-}
-catch (NullReferenceException ex)
-{
-    Console.WriteLine("Error: The reference is null." + ex.Message);
-}
-try
-{
-    int[] numbers = new int[5];
-    numbers[5] = 10;
-    Console.WriteLine("Number at index 5: " + numbers[5]);
-}
-catch (IndexOutOfRangeException ex)
-{
-    Console.WriteLine("Error: Index out of range." + ex.Message);
-}
-try
-{
-    int num3 = 10;
-    int num4 = 0;
-    int result2 = num3 / num4;
-    Console.WriteLine("Result: " + result2);
+    Workflow1(userEnteredValues);
+    Console.WriteLine("'Workflow1' completed successfully.");
+
 }
 catch (DivideByZeroException ex)
 {
-    Console.WriteLine("Error: Cannot divide by zero." + ex.Message);
+    Console.WriteLine("An error occurred during 'Workflow1'.");
+    Console.WriteLine(ex.Message);
 }
-Console.WriteLine("Exiting program.");
+
+static void Workflow1(string[][] userEnteredValues)
+{
+    foreach (string[] userEntries in userEnteredValues)
+    {
+        try
+        {
+            Process1(userEntries);
+            Console.WriteLine("'Process1' completed successfully.");
+            Console.WriteLine();
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("'Process1' encountered an issue, process aborted.");
+            Console.WriteLine(ex.Message);
+            Console.WriteLine();
+        }
+    }
+}
+
+static void Process1(String[] userEntries)
+{
+    int valueEntered;
+
+    foreach (string userValue in userEntries)
+    {
+        bool integerFormat = int.TryParse(userValue, out valueEntered);
+
+        if (integerFormat == true)
+        {
+            if (valueEntered != 0)
+            {
+                checked
+                {
+                    int calculatedValue = 4 / valueEntered;
+                }
+            }
+            else
+            {
+                throw new DivideByZeroException("Invalid data. User input values must be non-zero values.");
+            }
+        }
+        else
+        {
+            throw new FormatException("Invalid data. User input values must be valid integers.");
+        }
+    }
+}
